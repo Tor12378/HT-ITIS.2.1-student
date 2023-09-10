200cacc8b56df833150d140b5230fa0f757aeea6
Тимур <tupaev05@mail.ru>
2023-09-10 12:54:14 +0300

    First homework done

--- Homework1/Hw1/Parser.cs
+++ Homework1/Hw1/Parser.cs
@@ -2,18 +2,43 @@
 
 public static class Parser
 {
-    public static void ParseCalcArguments(string[] args, 
-        out double val1, 
-        out CalculatorOperation operation, 
+    public static void ParseCalcArguments(string[] args,
+        out double val1,
+        out CalculatorOperation operation,
         out double val2)
     {
-        throw new NotImplementedException();
+        if (IsArgLengthSupported(args))
+        {
+            val1 = ParseStringToDouble(args[0]);
+            operation = ParseOperation(args[1]);
+            val2 = ParseStringToDouble(args[2]);
+        }
+        else
+        {
+            throw new ArgumentException("Invalid number of arguments specified");
+        }
+    }
+
+    private static double ParseStringToDouble(string s)
+    {
+        if (double.TryParse(s, out var number))
+        {
+            return number;
+        }
+        throw new ArgumentException("The entered argument is not a number");
     }
 
     private static bool IsArgLengthSupported(string[] args) => args.Length == 3;
 
     private static CalculatorOperation ParseOperation(string arg)
     {
-        throw new NotImplementedException();
+        return arg switch
+        {
+            "+" => CalculatorOperation.Plus,
+            "-" => CalculatorOperation.Minus,
+            "*" => CalculatorOperation.Multiply,
+            "/" => CalculatorOperation.Divide,
+            _ => throw new InvalidOperationException("Operation specified incorrectly")
+        };
     }
 }
