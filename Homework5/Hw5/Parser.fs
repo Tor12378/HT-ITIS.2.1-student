b3f5bef3df7b8a935baa09e96266ade46ee89a78
Тимур <tupaev05@mail.ru>
2023-10-10 22:00:18 +0300

    5 hw done

--- Homework5/Hw5/Parser.fs
+++ Homework5/Hw5/Parser.fs
@@ -2,20 +2,46 @@
 
 open System
 open Hw5.Calculator
+open Hw5.MaybeBuilder
 
 let isArgLengthSupported (args:string[]): Result<'a,'b> =
-    (NotImplementedException() |> raise)
+    if(args.Length = 3) then
+        Ok args
+    else
+        Error Message.WrongArgLength
     
 [<System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage>]
 let inline isOperationSupported (arg1, operation, arg2): Result<('a * CalculatorOperation * 'b), Message> =
-    (NotImplementedException() |> raise)
-
+    match operation with
+    | Calculator.plus -> Ok (arg1, CalculatorOperation.Plus, arg2)
+    | Calculator.minus -> Ok (arg1,CalculatorOperation.Minus,arg2)
+    | Calculator.multiply -> Ok (arg1,CalculatorOperation.Multiply,arg2)
+    | Calculator.divide -> Ok (arg1,CalculatorOperation.Divide,arg2)
+    | _ -> Error Message.WrongArgFormatOperation
+let parseArgumentToDouble (arg: string) =
+    match Double.TryParse(arg) with
+    | true, num -> Ok num
+    | _ -> Error Message.WrongArgFormat
+    
 let parseArgs (args: string[]): Result<('a * CalculatorOperation * 'b), Message> =
-    (NotImplementedException() |> raise)    
+    maybe {
+        let! args = (args[0],args[1], args[2]) |> isOperationSupported
+        let! arg1 = args.Item1 |> parseArgumentToDouble
+        let! arg2 = args.Item3 |> parseArgumentToDouble
+        return (arg1, args.Item2, arg2)
+    }
 
 [<System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage>]
 let inline isDividingByZero (arg1, operation, arg2): Result<('a * CalculatorOperation * 'b), Message> =
-    (NotImplementedException() |> raise)       
+    if arg2 = 0.0 && operation = CalculatorOperation.Divide then
+        Error Message.DivideByZero
+    else
+        Ok (arg1, operation, arg2)     
     
 let parseCalcArguments (args: string[]): Result<'a, 'b> =
-    (NotImplementedException() |> raise)    
+    maybe { 
+        let! argLengthSupported = args |> isArgLengthSupported 
+        let! getParseArgs = argLengthSupported |> parseArgs
+        let! dividingNotByZero = getParseArgs |> isDividingByZero
+        return dividingNotByZero
+    }       
