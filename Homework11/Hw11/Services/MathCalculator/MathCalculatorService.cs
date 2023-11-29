f03f213a1c3c8f5711748e34eeae8a65acd41cf5
Тимур <tupaev05@mail.ru>
2023-11-29 22:44:38 +0300

    11 hw done

--- Homework11/Hw11/Services/MathCalculator/MathCalculatorService.cs
+++ Homework11/Hw11/Services/MathCalculator/MathCalculatorService.cs
@@ -1,9 +1,18 @@
+using System.Text.RegularExpressions;
+using Hw11.ExpressionHelper;
 namespace Hw11.Services.MathCalculator;
 
 public class MathCalculatorService : IMathCalculatorService
 {
     public async Task<double> CalculateMathExpressionAsync(string? expression)
     {
-        throw new NotImplementedException();
+        ExpressionValidator.Validate(expression);
+        var expressionParser = new ExpressionParser();
+        var split = new Regex("(?<=[-+*/()])|(?=[-+*/()])");
+        var expressionNotation = expressionParser.ParseToPolishNotation(split.Split(expression!));
+        var expressionConverted = ExpressionTreeConverter.ConvertToExpressionTree(expressionNotation);
+        await Task.Delay(1000);
+        var result = await MyExpressionVisitor.VisitExpression(expressionConverted);
+        return result;
     }
 }
