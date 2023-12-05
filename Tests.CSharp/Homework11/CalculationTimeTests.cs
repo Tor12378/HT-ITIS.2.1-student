42ac5edd43f649c7545b128b712d9a381e27f9ec
Тимур <tupaev05@mail.ru>
2023-12-05 07:26:03 +0300

    done hw12

--- Tests.CSharp/Homework11/CalculationTimeTests.cs
+++ Tests.CSharp/Homework11/CalculationTimeTests.cs
@@ -14,8 +14,8 @@ public class CalculationTimeTests : IClassFixture<WebApplicationFactory<Hw11.Pro
     }
 
     [HomeworkTheory(Homeworks.HomeWork11)]
-    [InlineData("2 + 3 + 4 + 6", 2000, 4000)]
-    [InlineData("(2 * 3 + 3 * 3) * (5 / 5 + 6 / 6)", 2000, 4000)]
+    [InlineData("2 + 3 + 4 + 6", 1999, 4000)]
+    [InlineData("(2 * 3 + 3 * 3) * (5 / 5 + 6 / 6)", 1999, 4000)]
     [InlineData("(2 + 3) / 12 * 7 + 8 * 9", 1500, 5000)]
     private async Task CalculatorController_ParallelTest(string expression, long minExpectedTime, long maxExpectedTime)
     {
